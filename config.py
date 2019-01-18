9154f86b05958cd2e30bd1c2a7f05897446638f4
Akkana Peck <akkana@shallowsky.com>
2019-01-17 20:34:58 -0700

    Mail settings

--- config.py
+++ config.py
@@ -7,3 +7,19 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# administrator list
+ADMINS = ['akkana@shallowsky.com']
+
+# Email server. This will fail except on a machine that's running
+# a mail server that allows local connections.
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+# MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
+
+# MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_USERNAME = "nmbilltracker"
+
+# MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
